65cdb8e837e1c262a7d706228d282851a17beae5
santiagg12 <santiirlanda@gmail.com>
2023-01-31 12:45:29 -0300

    m

--- src/app/components/service/educacion.service.ts
+++ src/app/components/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-URL = 'http://localhost:8080/educacion/';
+URL = 'https://backendms.onrender.com/educacion/';
 
   
   constructor(private httpclient : HttpClient) {  }
--- src/app/components/service/persona.service.ts
+++ src/app/components/service/persona.service.ts
@@ -9,7 +9,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-URL = 'http://localhost:8080/personas/';
+URL = 'https://backendms.onrender.com/personas/';
 
   constructor (private http: HttpClient) { }
 
