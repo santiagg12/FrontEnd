d6def36a78b91709825d31549f8d0d002f3d687e
santiagg12 <santiirlanda@gmail.com>
2023-01-31 16:52:02 -0300

    s

--- src/app/components/service/auth.service.ts
+++ src/app/components/service/auth.service.ts
@@ -9,7 +9,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'https://backendms.onrender.com/auth/'
+  authURL = 'https://backendms.onrender.com/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
