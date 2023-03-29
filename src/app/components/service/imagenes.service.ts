bb138cd23facce35137360aec3daa274caaded22
santiagg12 <santiirlanda@gmail.com>
2023-03-29 11:59:54 -0300

    img

--- src/app/components/service/imagenes.service.ts
+++ src/app/components/service/imagenes.service.ts
@@ -1,33 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Storage} from '@angular/fire/storage'
-import { async } from '@firebase/util';
-import { getDownloadURL, uploadBytes, list , getStorage, ref} from 'firebase/storage';
+import { Storage } from '@angular/fire/storage';
+import { getDownloadURL, uploadBytes, list , getStorage, ref } from 'firebase/storage';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImagenesService {
-  url: string = "";
-  storageFirebase = getStorage()
-  
+  private storageFirebase = getStorage();
+  public url: string = '';
+
   constructor(private storage: Storage) { }
 
-  public upLoadImage($event: any, nombre: string) {
-    const img = $event.target.files[0]
-    const imagenRef = ref(this.storageFirebase, nombre)
-    uploadBytes(imagenRef, img)
-      .then(Response => { alert("imagen subida correctamente") })
-      .catch(error => console.log(error))
+  public upLoadImage($event: any, nombre: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const img = $event.target.files[0];
+      const imagenRef = ref(this.storageFirebase, nombre);
+      uploadBytes(imagenRef, img)
+        .then(snapshot => {
+          getDownloadURL(snapshot.ref)
+            .then(url => {
+              this.url = url;
+              resolve(url);
+            })
+            .catch(error => reject(error));
+        })
+        .catch(error => reject(error));
+    });
   }
 
-  getImagen() {
-    const imgRef = ref(this.storage)
-    list(imgRef)
-      .then(async response => {
-        for (let item of response.items) {
-          this.url = await getDownloadURL(item)
-        }
-      }).catch(error => console.log(error)
-      )
+  public getImagen(): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const imgRef = ref(this.storage);
+      list(imgRef)
+        .then(async response => {
+          for (let item of response.items) {
+            this.url = await getDownloadURL(item);
+          }
+          resolve(this.url);
+        })
+        .catch(error => reject(error));
+    });
   }
 }
